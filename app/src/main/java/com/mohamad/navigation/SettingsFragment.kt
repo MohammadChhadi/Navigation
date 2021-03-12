7704f414fb0d46230fec715267a01766d4e9995d
mohamad chhadi <mohamad_chhadi@outlook.com>
2021-03-13 01:06:05 +0200

    Added update

--- app/src/main/java/com/mohamad/navigation/SettingsFragment.kt
+++ app/src/main/java/com/mohamad/navigation/SettingsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 
 class SettingsFragment : Fragment() {
     override fun onCreateView(
@@ -13,5 +14,13 @@ class SettingsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<View>(R.id.btLanguage).setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.languageFragment)
+        )
     }
 }
